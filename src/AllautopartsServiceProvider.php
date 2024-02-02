ef67ed2431d1819d0fd68c84ce8fea75e6475913
Sergey <1@php-cat.com>
2024-02-03 04:45:16 +0500

    1-0-7

--- src/AllautopartsServiceProvider.php
+++ src/AllautopartsServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Phpcatcom\Api\AllAutoParts;
+namespace Phpcatcom\Api;
 
 //use Illuminate\Support\Facades\Blade;
 use PhpCatCom\Middleware\AuthRoles;
--- src/Services/AllAutopartsService.php
+++ src/Services/AllAutopartsService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Phpcatcom\Api\AllAutoParts\Services;
+namespace Phpcatcom\Api\Services;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
